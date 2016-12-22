725485f72bc81e77d667c1afe480c891c0b85f55
juek <jk@grafikrausz.at>
2016-12-22 09:30:26 +0100

    add zoomEnabled class
    shows the zoom cursor (defined in custom style) only if the zoom image
    is larger than the current one.

--- ColorboxExtender.js
+++ ColorboxExtender.js
@@ -9,7 +9,18 @@ Version 1.0
 
 //* uses the event hook cbox_complete, see http://www.jacklmoore.com/colorbox
 $(document).on("cbox_complete", function(){
-  $("img.cboxPhoto").parent().zoom({ on : "grab" });
+  $("img.cboxPhoto").parent().zoom({ 
+    on : "grab", 
+    callback : function(){
+      var zoomImg = $(this);
+      var cboxPhoto = $(this).closest("#cboxLoadedContent").find(".cboxPhoto");
+      console.log("zoomImg.width() = " + zoomImg.width() + " | cboxPhoto.width() = " + cboxPhoto.width() );
+      if( zoomImg.width() != cboxPhoto.width() ){
+        // zoomImg.css({ cursor : "all-scroll" });
+        zoomImg.addClass("zoomEnabled");
+      }
+    }
+  });
 });
 //*/
 
