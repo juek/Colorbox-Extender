11de833c611b3ad75550db7d968523f1b07a4f14
tf <tf@webks.de>
2016-12-23 09:45:31 +0100

    Code cleanup

--- extensions/pinch_swipe/init.js
+++ extensions/pinch_swipe/init.js
@@ -1,94 +1,93 @@
 // TODO: Add optional instructions "message / image" for swipe & pinchzoom (using background images so users can easily override it via css)
+// TODO: Check if the cbox content is an image otherwise
 function hammerIt(elm) { // Based on: http://stackoverflow.com/a/27572427/6266306
-    hammertime = new Hammer(elm, {});
-    hammertime.get('pinch').set({
-        enable: true
-    });
-    var posX = 0,
-        posY = 0,
-        scale = 1,
-        last_scale = 1,
-        last_posX = 0,
-        last_posY = 0,
-        max_pos_x = 0,
-        max_pos_y = 0,
-        transform = "",
-        el = elm;
+  hammertime = new Hammer(elm, {});
+  hammertime.get('pinch').set({
+    enable: true
+  });
+  var posX = 0,
+  posY = 0,
+  scale = 1,
+  last_scale = 1,
+  last_posX = 0,
+  last_posY = 0,
+  max_pos_x = 0,
+  max_pos_y = 0,
+  transform = "",
+  el = elm;
 
-    hammertime.on('doubletap pan pinch panend pinchend', function(ev) {
-        if (ev.type == "doubletap") {
-          console.log('double tapped!');
-          if(last_scale == 1){
-            transform =
-                "translate3d(0, 0, 0) " +
-                "scale3d(2, 2, 1) ";
-            scale = 2;
-            last_scale = 2;
-          }else if(last_scale == 2){
-            // TODO: Descover why the default if (see stackoverflow) is not working properly, as the simple last_scale check fails if pinchzoom is performed before | if (window.getComputedStyle(el, null).getPropertyValue('transform').toString() != "matrix(1, 0, 0, 1, 0, 0)") {
-            transform =
-                    "translate3d(0, 0, 0) " +
-                    "scale3d(1, 1, 1) ";
-                scale = 1;
-                last_scale = 1;
+  hammertime.on('doubletap pan pinch panend pinchend', function(ev) {
+    if (ev.type == "doubletap") {
+      console.log('double tapped!');
+      if(last_scale == 1){
+        transform =
+        "translate3d(0, 0, 0) " +
+        "scale3d(2, 2, 1) ";
+        scale = 2;
+        last_scale = 2;
+      }else if(last_scale == 2){
+        // TODO: Discover why the default if (see stackoverflow) is not working properly, as the simple last_scale check fails if pinchzoom is performed before | if (window.getComputedStyle(el, null).getPropertyValue('transform').toString() != "matrix(1, 0, 0, 1, 0, 0)") {
+        transform =
+        "translate3d(0, 0, 0) " +
+        "scale3d(1, 1, 1) ";
+        scale = 1;
+        last_scale = 1;
 
-          }else{
-            console.error('last_scale undefined!');
-          }
-          el.style.transform = transform;
-          transform = "";
-        }
+      }else{
+        console.error('last_scale undefined!');
+      }
+      el.style.transform = transform;
+      transform = "";
+    }
 
-        //pan
-        if (scale != 1) {
-            posX = last_posX + ev.deltaX;
-            posY = last_posY + ev.deltaY;
-            max_pos_x = Math.ceil((scale - 1) * el.clientWidth / 2);
-            max_pos_y = Math.ceil((scale - 1) * el.clientHeight / 2);
-            if (posX > max_pos_x) {
-                posX = max_pos_x;
-            }
-            if (posX < -max_pos_x) {
-                posX = -max_pos_x;
-            }
-            if (posY > max_pos_y) {
-                posY = max_pos_y;
-            }
-            if (posY < -max_pos_y) {
-                posY = -max_pos_y;
-            }
-        }
+    //pan
+    if (scale != 1) {
+      posX = last_posX + ev.deltaX;
+      posY = last_posY + ev.deltaY;
+      max_pos_x = Math.ceil((scale - 1) * el.clientWidth / 2);
+      max_pos_y = Math.ceil((scale - 1) * el.clientHeight / 2);
+      if (posX > max_pos_x) {
+        posX = max_pos_x;
+      }
+      if (posX < -max_pos_x) {
+        posX = -max_pos_x;
+      }
+      if (posY > max_pos_y) {
+        posY = max_pos_y;
+      }
+      if (posY < -max_pos_y) {
+        posY = -max_pos_y;
+      }
+    }
 
 
-        //pinch
-        if (ev.type == "pinch") {
-            scale = Math.max(.999, Math.min(last_scale * (ev.scale), 4));
-        }
-        if(ev.type == "pinchend"){last_scale = scale;}
+    //pinch
+    if (ev.type == "pinch") {
+      scale = Math.max(.999, Math.min(last_scale * (ev.scale), 4));
+    }
+    if(ev.type == "pinchend"){last_scale = scale;}
 
-        //panend
-        if(ev.type == "panend"){
-            last_posX = posX < max_pos_x ? posX : max_pos_x;
-            last_posY = posY < max_pos_y ? posY : max_pos_y;
-        }
+    //panend
+    if(ev.type == "panend"){
+      last_posX = posX < max_pos_x ? posX : max_pos_x;
+      last_posY = posY < max_pos_y ? posY : max_pos_y;
+    }
 
-        if (scale != 1) {
-            transform =
-                "translate3d(" + posX + "px," + posY + "px, 0) " +
-                "scale3d(" + scale + ", " + scale + ", 1)";
-        }
+    if (scale != 1) {
+      transform =
+      "translate3d(" + posX + "px," + posY + "px, 0) " +
+      "scale3d(" + scale + ", " + scale + ", 1)";
+    }
 
-        if (transform) {
-            el.style.transform = transform;
-        }
-    });
+    if (transform) {
+      el.style.transform = transform;
+    }
+  });
 }
 
-function cboxClassCleanup(message) {
-  console.log('class cleanup!' + message);
+function cboxClassCleanup() {
   $cboxWrapper.removeClass(swipeOutLeftClass).removeClass(swipeOutRightClass).removeClass(swipeInLeftClass).removeClass(swipeInRightClass);
 }
-
 function cboxSwipeLeft() {
   cboxClassCleanup('cboxSwipeLeft');
   if(!$cboxWrapper.hasClass(swipeOutLeftClass)){
@@ -112,12 +111,10 @@ function cboxSwipeUp() {
 function cboxSwipeDown() {
   cboxClassCleanup('cboxSwipeDown');
   $cboxWrapper.addClass(swipeOutDownClass);
-  lastDirection = "up";
+  lastDirection = "down";
   $.colorbox.close();
 }
-
 $(document).on('cbox_open', function () {
-  console.log("cbox_open fired: set variables ..doing stuff and things");
   $cboxWrapper = $("#colorbox:first");
   $cboxContentWrapper = $cboxWrapper.find('#cboxLoadedContent:first');
 
@@ -139,7 +136,7 @@ $(document).on('cbox_open', function () {
       $cboxWrapper.find('.cboxPhoto').wrap('<div id="cboxZoomImageWrapper" class="colorbox__pinchzoom-wrapper"></div>');
 
       $cboxWrapper.addClass("colorbox--pinchzoom-active");
-      // Prevent image dialog
+      // Prevent image dialog on longtap (browser context menu)
       $cboxWrapper.find('.cboxPhoto').hammer().on('touchstart', function (e) {
         e.preventDefault()
       });
@@ -150,45 +147,31 @@ $(document).on('cbox_open', function () {
   $cboxWrapper.hammer().on('swipeleft', function (event, data) {
     if (event.gesture) {
       event.gesture.srcEvent.stopPropagation();
-      console.log('hammerSwipeLeft!');
-      console.log(event);
       cboxSwipeLeft();
-      // prevent swipe fired multiple times
-      // $cboxWrapper.hammer().off('swipeleft');
     }
     return false;
   });
   $cboxWrapper.hammer().on('swiperight', function (event, data) {
     if (event.gesture) {
       event.gesture.srcEvent.stopPropagation();
-      console.log('hammerSwipeRight!');
       cboxSwipeRight();
     }
     return false;
   });
-
   $cboxWrapper.hammer().on('swipeup', function (event, data) {
     if (event.gesture) {
       event.gesture.srcEvent.stopPropagation();
-      console.log('hammerSwipeUP!');
       cboxSwipeUp();
     }
-      return false;
+    return false;
   });
   $cboxWrapper.hammer().on('swipedown', function (event, data) {
     if (event.gesture) {
       event.gesture.srcEvent.stopPropagation();
-      console.log('hammerSwipeDown!');
       cboxSwipeDown();
     }
     return false;
   });
-
-});
-
-$(document).on('cbox_load', function () {
-  console.log("cbox_load");
-  // moved "swipe in again" scripts to cbox_complete as cbox_load fires to early? (buggy in chrome)
 });
 
 $(document).on('cbox_complete', function () {
@@ -200,11 +183,9 @@ $(document).on('cbox_complete', function () {
 
   // Determine from which direction we have to swipe in again
   if (lastDirection === "left") {
-    console.log('perform swipe in from the right side!');
     cboxClassCleanup('lastDirectionSwipeFromRight');
     $cboxWrapper.addClass(swipeInRightClass);
   } else if (lastDirection === "right") {
-    console.log('perform swipe in from the left side!');
     cboxClassCleanup('lastDirectionSwipeFromLeft');
     $cboxWrapper.addClass(swipeInLeftClass);
   }
