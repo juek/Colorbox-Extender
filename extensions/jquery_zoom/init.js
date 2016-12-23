2371e981e548680e7e7bfed5babad568d5717a6d
juek <jk@grafikrausz.at>
2016-12-23 10:11:12 +0100

    update zoom cursor on win.resize

--- extensions/jquery_zoom/init.js
+++ extensions/jquery_zoom/init.js
@@ -2,16 +2,24 @@
 $(document).on("cbox_complete", function(){
   $("img.cboxPhoto").parent().zoom({ 
     on : "grab", 
-    callback : function(){
-      var zoomImg = $(this);
-      var cboxPhoto = $(this).closest("#cboxLoadedContent").find(".cboxPhoto");
-      console.log("zoomImg.width() = " + zoomImg.width() + " | cboxPhoto.width() = " + cboxPhoto.width() );
-      if( zoomImg.width() != cboxPhoto.width() ){
-        // zoomImg.css({ cursor : "all-scroll" });
-        zoomImg.addClass("zoomEnabled");
-      }
-    }
+    callback : cboxZoom_setCursor
   });
 });
 //*/
 
+$(window).on("resize", function(){
+  cboxZoom_setCursor.call( $("#colorbox .zoomImg") );
+});
+
+function cboxZoom_setCursor(){
+  var zoomImg = $(this);
+  var cboxPhoto = $(this).closest("#cboxLoadedContent").find(".cboxPhoto");
+  // console.log("zoomImg.width() = " + zoomImg.width() + " | cboxPhoto.width() = " + cboxPhoto.width() );
+  if( zoomImg.width() != cboxPhoto.width() ){
+    zoomImg.css({ cursor : "all-scroll" , cursor : "zoom-in" });
+    zoomImg.addClass("zoomEnabled");
+  }else{
+    zoomImg.css({ cursor : "default" });
+    zoomImg.removeClass("zoomEnabled");
+  }
+}
