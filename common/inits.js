f596bd9aa74341fdb6539eb4a8d2f9ceb35c57a3
juek <jk@grafikrausz.at>
2019-08-09 17:24:29 +0200

    restore original settings onClosed

--- common/inits.js
+++ common/inits.js
@@ -1,18 +1,22 @@
 /* additional common Colorbox loading bindings  */
 
-$(document).ready( function(){
+window.colorbox_lang_backup = JSON.stringify(colorbox_lang);
 
-  //* open any link in Colorbox that has the 'colorbox' class
+$(function(){
+
+  // open any link in Colorbox that has the 'colorbox' class
   $("a.colorbox").colorbox( $gp.cboxSettings() );
-  //*/
+  //
 
-  //* loads any Typesetter page into Colorbox
-  $(".loadPageInColorbox").on("click", function(e){
+  // loads any Typesetter page into Colorbox
+  $("a.loadPageInColorbox, GPAREA.loadPageInColorbox a").on("click", function(e){
     if( isadmin ){
       var edit_page = confirm("You are logged in.\n\n" 
         + "Click OK, to load the target page for editing.\n" 
         + "Click Cancel, to show the page content in Colorbox.");
-      if( edit_page ){ return true; }
+      if( edit_page ){
+        return true;
+      }
     }
     e.preventDefault();
     var src = jPrep(this.href, 'gpreq=body');
@@ -23,8 +27,11 @@ $(document).ready( function(){
       iframe : true, 
       fastIframe : false,
       width : "90%",
-      height : "90%" // ,
-      // onOpen : function() { 
+      height : "90%",
+      onClosed : function(){ 
+       colorbox_lang = JSON.parse(colorbox_lang_backup);
+      }// ,
+      // onOpen : function(){ 
         // do sth. 
       // },
       // onComplete : function(){ 
@@ -34,10 +41,5 @@ $(document).ready( function(){
     // console.log(options);
     $.colorbox(options);
   });
-  //*/
 
-}); /* domready --end */
-
-
-
-
+});
